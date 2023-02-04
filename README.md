bd67d3487efed61814b6308d066e21a58d476fef
vampir7421 <vampir7421@gmail.com>
2023-02-04 17:59:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-b2f5wn-sbcsqb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-b2f5wn-xfn7rp)
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>1233</h1>
+    <div>11dd1asd</div>
+    <h2>123</h2>
+    <h4>111</h4>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
